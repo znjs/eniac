61433ab0712fd4161def7961ecca397e684d5a7b
Rohan Mondal <mondalrohan108@gmail.com>
2022-05-08 17:23:51 +0530

    fixed small bugs

--- src/context/interviews/interviewContext.js
+++ src/context/interviews/interviewContext.js
@@ -1,10 +1,17 @@
-import { createContext, useContext, useReducer, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { initialState, reducer } from "../../reducer";
 
 const InterviewContext = createContext();
 
 const InterviewContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {}, []);
   return (
     <InterviewContext.Provider value={{ state, dispatch, initialState }}>
       {children}
